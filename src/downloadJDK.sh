478b5a6065a054c40c70fcafca20683ae7c6e19d
Rubens Neto <rubens@vertigo.com.br>
2016-04-09 14:49:12 -0300

    jdk upg

--- src/downloadJDK.sh
+++ src/downloadJDK.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+JDKVERSION=8u77
 echo "downloading JDK..."
-CODE=$(curl -I http://registry.vtg/files/oracle/jdk-8u72-linux-x64.rpm 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+CODE=$(curl -I http://registry.vtg/files/oracle/jdk-$JDKVERSION-linux-x64.rpm 2>/dev/null | head -n 1 | cut -d$' ' -f2)
 if [ "$CODE" == "200" ]; then
-    wget http://registry.vtg/files/oracle/jdk-8u72-linux-x64.rpm -q -O /opt/jdk.rpm
+    wget http://registry.vtg/files/oracle/jdk-$JDKVERSION-linux-x64.rpm -q -O /opt/jdk.rpm
 else
     wget --no-check-certificate \
        --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-       http://download.oracle.com/otn-pub/java/jdk/8u72-b15/jdk-8u72-linux-x64.rpm -q -O /opt/jdk.rpm
+       http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.rpm \
+       -q -O /opt/jdk.rpm
 fi
 echo "...JDK downloaded!"
